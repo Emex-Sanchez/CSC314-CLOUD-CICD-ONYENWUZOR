80414ae91a06fbe2d6fa0cebfea8ef17c3c47fda
Emmanuel <onyenwuzorce@gmail.com>
2024-06-03 16:44:39 +0100

    Made a few more changes

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 8080;
-app.use(express.static("AWS"));
+app.use(express.static("static web"));
 
-// app.get('/', (req, res) => res.send(' this is CSS 314 cloud computing DEVOPS ; CI-CD PIPELINE. THIS PAGE INDICATES SUCCESS  !'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE STEP 1 !'));
+//app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE STEP 1 !'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/about.html'));
-});
-
-
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
+console.log('THIS IS CSC314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS, THIS PAGE INDICATES SUCCESS \n Emmanuel C Onyenwuzor \n VUG/CSC/21/6280');
